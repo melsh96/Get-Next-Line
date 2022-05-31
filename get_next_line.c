502398f19afbe322d1c09e1f74b36849ef577a90
Melodie SHAHRVARI <meshahrv@e3r10p14.clusters.42paris.fr>
2022-05-31 14:55:52 +0200

    GNL : review

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,13 @@
 /*   By: meshahrv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 13:10:51 by meshahrv          #+#    #+#             */
-/*   Updated: 2022/05/30 16:44:57 by meshahrv         ###   ########.fr       */
+/*   Updated: 2022/05/31 14:30:56 by meshahrv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// char			*buf[BUFFER_SIZE + 1];
 char	*get_next_line(int fd)
 {
 	static t_list	*stash;
@@ -21,7 +22,27 @@ char	*get_next_line(int fd)
 	i = 0;
 	stash = ft_lstnew(stash->content);
 	line[i] = '\0';
-	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)
 		return (NULL);
 	while ()
+	{
+		
+	}
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("test", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		printf("%s\n", line);
+		if (line == NULL)
+			break ;
+		free(line);
+	}
+	return (0);
 }
